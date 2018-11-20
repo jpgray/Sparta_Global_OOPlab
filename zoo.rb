25b6c86838ec7d8789085052ffabf21767834d47
Jon Gray <jgray@spartaglobal.com>
2018-11-20 11:12:57 +0000

    Zoo inhabitants - over arching class with some attributes

---
+++ zoo.rb
@@ -0,0 +1,32 @@
+class ZooInhabitant
+  def initialize(name = "Unknown", age = "Unknown", favouriteFood = "Pizza!")
+    @name = name
+    @age = age
+    @favouriteFood = favouriteFood
+  end
+
+  def location
+    puts "Currently at the SuperZoo"
+  end
+
+  def name
+    puts "#{@name}"
+  end
+
+  def play
+    puts "#{@name} is playing!"
+  end
+
+  def age
+    puts "#{@age}"
+  end
+
+  def favouriteFood
+    puts "#{@favouriteFood}"
+  end
+end
+
+Bob = ZooInhabitant.new "Bob", 24, "Hot Dogs!"
+Bob.play
+Bob.favouriteFood
+Bob.age
