db9688f22a0372a85a1e6ccb395cf74541ca9b19
Jon Gray <jgray@spartaglobal.com>
2018-11-20 11:34:25 +0000

    v0.2 working but imperfect Staff class

---
+++ staff.rb
@@ -0,0 +1,27 @@
+require_relative './zoo.rb'
+class Staff < ZooInhabitant
+  def initialize(name = "Staff Member", age = "16+", favouriteFood = "Pizza!", team = "general staff", subrole = "Administrative/ customer support")
+    # super(name)
+    # super(age)
+    # super(favouriteFood)
+
+    # super
+
+    @name = name
+    @age = age
+    @favouriteFood = favouriteFood
+    @team = team
+    @subrole = subrole
+  end
+
+  def role
+    puts "#{@name} is in the #{@team} team and is tasked with #{@subrole}"
+  end
+
+end
+
+sandra = Staff.new "Sandra", 28, "Flammenkuchen!"
+sandra.role
+sandra.name
+sandra.age
+sandra.favouriteFood
